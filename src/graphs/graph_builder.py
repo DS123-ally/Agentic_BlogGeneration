aa82499aaac664da1742116494f1d5bf7fba6629
DS123-ally <dineshseervi33716@gmail.com>
2026-04-15 18:08:43 +0530

    update

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -15,13 +15,29 @@ class GraphBuilder:
         """    
         self.blog_node_obj=BlogNode(self.llm)
         ##Nodes
-        self.graph.add_node("title_creations",self.blog_node_obj.title_creation)
+        self.graph.add_node("title_creation",self.blog_node_obj.title_creation)
         self.graph.add_node("content_generation",self.blog_node_obj.content_generation)
 
         ##Edges
-        self.graph.add_edge(START,"title_creations")
-        self.graph.add_edge("title_creations","content_generations")
-        self.graph.add_edge("content_generations",END)
+        self.graph.add_edge(START,"title_creation")
+        self.graph.add_edge("title_creation","content_generation")
+        self.graph.add_edge("content_generation",END)
 
         return self.graph
+    
+
+    def setup_graph(self,usecase):
+        if usecase=="topic":
+            self.build_topic_graph()
+
+        return self.graph.compile()
+
+
+#Langsmith Studio
+
+llm=GroqLLM().get_llm()    
+
+graph_builder=GraphBuilder(llm)
+graph=graph_builder.build_topic_graph().compile()
+
 
